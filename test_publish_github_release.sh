366fe4a645a766d4c2623c85ea69d2dc86242c58
Niraj Jayant <niraj@bolt.com>
2019-12-11 12:58:38 -0800

    Fixing tests

--- test_publish_github_release.sh
+++ test_publish_github_release.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
 
-while true; do
-        mkdir -p /tmp/swissknife/
-        wget -P /tmp/swissknife/ -qi - https://github.com/aktau/github-release/releases/download/v0.7.2/darwin-amd64-github-release.tar.bz2
+set -ex
+echo "Running test"
+mkdir -p /tmp/swissknife/
+wget -P /tmp/swissknife/ -q "https://github.com/aktau/github-release/releases/download/v0.7.2/darwin-amd64-github-release.tar.bz2"
 
-        tar xjf /tmp/swissknife/*.tar.bz2 -C /tmp/swissknife
-        now=$(date +"%T")
-        /tmp/swissknife/bin/linux/amd64/github-release release \
-          --security-token $GITHUB_TOKEN \
-          --user "BoltApp" \
-          --repo "sleet" \
-          --tag v1.0.0-$now
-done
+echo "Unpacking file"
+tar xjf /tmp/swissknife/*.tar.bz2 -C /tmp/swissknife
+now=$(date +"%s%N")
+echo "Now . " + $now
+/tmp/swissknife/bin/darwin/amd64/github-release release \
+  --security-token $GITHUB_TOKEN \
+  --user "BoltApp" \
+  --repo "sleet" \
+  --tag v1.0.1-$now

f8d7d9702c9b2d119f44956ae906c6bc2e3489b0
nirajjayantbolt <41930082+nirajjayantbolt@users.noreply.github.com>
2019-12-11 15:15:02 -0800

    Github Release Orb (#23)
    * [GitHub release orb]

---
+++ test_publish_github_release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+GITHUB_REPO="sleet"
+GITHUB_USER="BoltApp"
+
+set -ex
+echo "Running test"
+mkdir -p /tmp/swissknife/
+wget -P /tmp/swissknife/ -q "https://github.com/aktau/github-release/releases/download/v0.7.2/darwin-amd64-github-release.tar.bz2"
+
+echo "Unpacking file"
+tar xjf /tmp/swissknife/*.tar.bz2 -C /tmp/swissknife
+now=$(date +"%s")
+echo "Now . " + $now
+/tmp/swissknife/bin/darwin/amd64/github-release release \
+  --security-token $GITHUB_TOKEN \
+  --user "$GITHUB_USER" \
+  --repo "$GITHUB_REPO" \
+  --tag v1.0.1-$now

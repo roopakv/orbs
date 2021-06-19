ab5cd776b28bb0fa246f1d7688c051a6fc368317
Ryan Albert <42415738+ryan-timothy-albert@users.noreply.github.com>
2021-06-19 13:53:29 -0700

    Use Bash Script for Modified Files (#85)
    * Add Modified Files Command
    
    * Address Comments
    
    * Address Comments
    
    * Modified Files Bash Script
    
    * new line
    
    * Job Title

---
+++ scripts/modified-files.sh
@@ -0,0 +1,23 @@
+if [ -z "$BASH" ]; then
+    echo Bash not installed.
+    exit 1
+fi
+git status >/dev/null 2>&1 || { echo >&2 "Not in a git directory or no git"; exit 1; }
+
+mkdir -p /tmp/swissknife
+touch /tmp/swissknife/modified_files.txt
+
+FILES_MODIFIED=""
+get_modified_files() {
+    FILES_MODIFIED=""
+    FILES_MODIFIED=$(git diff --name-only $(git merge-base HEAD origin/${BASE_BRANCH})..HEAD)
+}
+
+get_modified_files
+if [ -z "$FILES_MODIFIED" ]
+then
+    echo "Files not modified"
+else
+    echo "$FILES_MODIFIED" >> /tmp/swissknife/modified_files.txt
+    cat /tmp/swissknife/modified_files.txt
+fi

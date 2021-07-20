f8e3773719bad58d7c2040496483dd06a005e3ab
russelltaga <russell@coastapp.com>
2021-07-20 10:13:55 -0700

    trigger-pipeline tag support #89 (#90)
    * trigger-pipeline tag support #89

--- scripts/trigger-pipeline.sh
+++ scripts/trigger-pipeline.sh
@@ -19,18 +19,28 @@ cat > /tmp/swissknife/trigger_pipeline.sh <<'EOF'
   export vcs_type="$1";
   export username="$2";
   export reponame="$3";
-  export branch="$4";
-  export params="$5";
+  if [[ $4 == "-t" ]]; then
+    export tag="$5";
+    export params="$6";
+    export body='{
+      "tag": "'$tag'",
+      "parameters": '"$params"'
+    }';
+  else
+    export branch="$4";
+    export params="$5";
+    export body='{
+      "branch": "'$branch'",
+      "parameters": '"$params"'
+    }';
+  fi
 
   trigger_workflow() {
     curl --silent -X POST \
       "https://circleci.com/api/v2/project/$vcs_type/$username/$reponame/pipeline?circle-token=${CIRCLE_TOKEN}" \
       -H 'Accept: */*' \
       -H 'Content-Type: application/json' \
-      -d '{
-        "branch": "'$branch'",
-        "parameters": '"$params"'
-      }'
+      -d "${body}"
   }
 
   trigger_workflow
@@ -44,7 +54,12 @@ chmod +x /tmp/swissknife/trigger_pipeline.sh
 PARAM_USER=$(printf '%s\n' "${!PARAM_USER_ENV_VAR}")
 PARAM_REPO=$(printf '%s\n' "${!PARAM_REPO_ENV_VAR}")
 PARAM_BRANCH=$(printf '%s\n' "${!PARAM_BRANCH_ENV_VAR}")
+PARAM_TAG=$(printf '%s\n' "${!PARAM_TAG_ENV_VAR}")
 
 if [[ "$SKIP_TRIGGER" == "0" || "$SKIP_TRIGGER" == "false" ]]; then
-  /tmp/swissknife/trigger_pipeline.sh "$VCS_TYPE" "$PARAM_USER" "$PARAM_REPO" "$PARAM_BRANCH" "$CUSTOM_PARAMS"
+  if [[ "$PARAM_BRANCH_ENV_VAR" != "" ]]; then
+    /tmp/swissknife/trigger_pipeline.sh "$VCS_TYPE" "$PARAM_USER" "$PARAM_REPO" "$PARAM_BRANCH" "$CUSTOM_PARAMS"
+  else
+    /tmp/swissknife/trigger_pipeline.sh "$VCS_TYPE" "$PARAM_USER" "$PARAM_REPO" -t "$PARAM_TAG" "$CUSTOM_PARAMS"
+  fi
 fi

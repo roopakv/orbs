9d0b0bdc7d6a4dba1651889df594df6dd8b33e2d
Russell Taga <russell@coastapp.com>
2021-07-19 21:10:57 -0700

    trigger-pipeline tag support #89

--- scripts/trigger-pipeline.sh
+++ scripts/trigger-pipeline.sh
@@ -11,6 +11,7 @@ fi
 
 mkdir -p /tmp/swissknife/
 
+if [[ "$PARAM_BRANCH_ENV_VAR" != "" ]]; then
 cat > /tmp/swissknife/trigger_pipeline.sh <<'EOF'
   #!/bin/bash -x
   echo "----------------------------------------"
@@ -38,13 +39,47 @@ cat > /tmp/swissknife/trigger_pipeline.sh <<'EOF'
   echo "Finished triggering pipeline"
   echo "----------------------------------------"
 EOF
+else
+cat > /tmp/swissknife/trigger_pipeline.sh <<'EOF'
+  #!/bin/bash -x
+  echo "----------------------------------------"
+  echo "Triggering Pipeline"
+
+  export vcs_type="$1";
+  export username="$2";
+  export reponame="$3";
+  export tag="$4";
+  export params="$5";
+
+  trigger_workflow() {
+    curl --silent -X POST \
+      "https://circleci.com/api/v2/project/$vcs_type/$username/$reponame/pipeline?circle-token=${CIRCLE_TOKEN}" \
+      -H 'Accept: */*' \
+      -H 'Content-Type: application/json' \
+      -d '{
+        "tag": "'$tag'",
+        "parameters": '"$params"'
+      }'
+  }
+
+  trigger_workflow
+
+  echo "Finished triggering pipeline"
+  echo "----------------------------------------"
+EOF
+fi
 
 chmod +x /tmp/swissknife/trigger_pipeline.sh
 
 PARAM_USER=$(printf '%s\n' "${!PARAM_USER_ENV_VAR}")
 PARAM_REPO=$(printf '%s\n' "${!PARAM_REPO_ENV_VAR}")
 PARAM_BRANCH=$(printf '%s\n' "${!PARAM_BRANCH_ENV_VAR}")
+PARAM_TAG=$(printf '%s\n' "${!PARAM_TAG_ENV_VAR}")
 
 if [[ "$SKIP_TRIGGER" == "0" || "$SKIP_TRIGGER" == "false" ]]; then
-  /tmp/swissknife/trigger_pipeline.sh "$VCS_TYPE" "$PARAM_USER" "$PARAM_REPO" "$PARAM_BRANCH" "$CUSTOM_PARAMS"
+  if [[ "$PARAM_BRANCH_ENV_VAR" != "" ]]; then
+    /tmp/swissknife/trigger_pipeline.sh "$VCS_TYPE" "$PARAM_USER" "$PARAM_REPO" "$PARAM_BRANCH" "$CUSTOM_PARAMS"
+  else
+    /tmp/swissknife/trigger_pipeline.sh "$VCS_TYPE" "$PARAM_USER" "$PARAM_REPO" "$PARAM_TAG" "$CUSTOM_PARAMS"
+  fi
 fi

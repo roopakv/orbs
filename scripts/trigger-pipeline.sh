c50f4cc113cee7a6df132218566e78fab023d399
russelltaga <russell@coastapp.com>
2021-07-21 09:08:34 -0700

    Fix conditional #89

--- scripts/trigger-pipeline.sh
+++ scripts/trigger-pipeline.sh
@@ -57,7 +57,7 @@ PARAM_BRANCH=$(printf '%s\n' "${!PARAM_BRANCH_ENV_VAR}")
 PARAM_TAG=$(printf '%s\n' "${!PARAM_TAG_ENV_VAR}")
 
 if [[ "$SKIP_TRIGGER" == "0" || "$SKIP_TRIGGER" == "false" ]]; then
-  if [[ "$PARAM_BRANCH_ENV_VAR" != "" ]]; then
+  if [[ "$PARAM_TAG" == "" ]]; then
     /tmp/swissknife/trigger_pipeline.sh "$VCS_TYPE" "$PARAM_USER" "$PARAM_REPO" "$PARAM_BRANCH" "$CUSTOM_PARAMS"
   else
     /tmp/swissknife/trigger_pipeline.sh "$VCS_TYPE" "$PARAM_USER" "$PARAM_REPO" -t "$PARAM_TAG" "$CUSTOM_PARAMS"

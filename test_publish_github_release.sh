75006ca41791977211048e6ee5c674a666395575
Robert Cooper <rlcooper46@gmail.com>
2022-04-04 11:50:19 -0700

    change link for github-release (#102)

--- test_publish_github_release.sh
+++ test_publish_github_release.sh
@@ -6,7 +6,7 @@ GITHUB_USER="BoltApp"
 set -ex
 echo "Running test"
 mkdir -p /tmp/swissknife/
-wget -P /tmp/swissknife/ -q "https://github.com/aktau/github-release/releases/download/v0.7.2/darwin-amd64-github-release.tar.bz2"
+wget -P /tmp/swissknife/ -q "https://github.com/github-release/github-release/releases/download/v0.7.2/darwin-amd64-github-release.tar.bz2"
 
 echo "Unpacking file"
 tar xjf /tmp/swissknife/*.tar.bz2 -C /tmp/swissknife
